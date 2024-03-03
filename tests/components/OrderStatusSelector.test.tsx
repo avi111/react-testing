90306cec3129c3f6be0a911c57410b68663fda16
avi111 <avi111@gmail.com>
2024-03-03 20:23:38 +0200

    finished react components

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 
 import OrderStatusSelector from '../../src/components/OrderStatusSelector';
 import {Theme} from "@radix-ui/themes";
-import {expect} from "vitest";
+import {describe, expect} from "vitest";
 import {userEvent} from "@testing-library/user-event";
 
 describe('OrderStatusSelector', () => {
@@ -15,7 +15,10 @@ describe('OrderStatusSelector', () => {
         );
         const select = screen.getByRole('combobox');
         const getOptions = () => screen.getAllByRole('option');
-        return {select, onChange, getOptions};
+        const user = userEvent.setup();
+        const getOption = (label: RegExp) => screen.findByRole('option', {name: label});
+
+        return {select, onChange, getOptions, user, getOption};
     }
     it('should render the order status selector', async () => {
         const {select} = renderOrderStatusSelector();
@@ -28,8 +31,7 @@ describe('OrderStatusSelector', () => {
     });
 
     it('should render the correct values', async () => {
-        const {select, getOptions} = renderOrderStatusSelector();
-        const user = userEvent.setup();
+        const {select, getOptions, user} = renderOrderStatusSelector();
         await user.click(select);
 
         const options = getOptions();
@@ -37,5 +39,32 @@ describe('OrderStatusSelector', () => {
 
         const labels = options.map(option => option.textContent);
         expect(labels).toEqual(['New', 'Processed', 'Fulfilled']);
+    });
+
+    it.each([
+        {label: /processed/i, value: 'processed'},
+        {label: /fulfilled/i, value: 'fulfilled'},
+    ])(`should call the onChange callback with $value when the $label is selected`, async ({label, value}) => {
+        const {select, onChange, user, getOption} = renderOrderStatusSelector();
+        await user.click(select);
+
+        const option = await getOption(label);
+        await user.click(option);
+
+        expect(onChange).toHaveBeenCalledWith(value);
+    });
+
+    it('should call the onChange callback with new when the new option is selected', async () => {
+        const {select, onChange, user, getOption} = renderOrderStatusSelector();
+
+        await user.click(select);
+        const processedOption = await getOption(/processed/i);
+        await user.click(processedOption);
+
+        await user.click(select);
+        const newOption = await getOption(/new/i);
+        await user.click(newOption);
+
+        expect(onChange).toHaveBeenCalledWith('new');
     });
 });
