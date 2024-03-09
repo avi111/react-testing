4cd787dbca1801034401e07150431f587dd554ba
avi111 <avi111@gmail.com>
2024-03-09 20:01:06 +0200

    mocking apis - end of lecture 12

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,7 +1,7 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react';
 
 import ProductDetail from '../../src/components/ProductDetail';
-import {http, HttpResponse} from "msw";
+import {delay, http, HttpResponse} from "msw";
 import {server} from "../mocks/server.ts";
 import {afterAll, beforeAll} from "vitest";
 import {db} from "../mocks/db.ts";
@@ -41,5 +41,46 @@ describe('ProductDetail', () => {
 
         const message = await screen.findByText(/invalid/i);
         expect(message).toBeInTheDocument();
+    });
+
+    it('should render an error message when an unexpected error occurs', async () => {
+        server.use(http.get('/products/1', () => HttpResponse.error()))
+        render(<ProductDetail productId={1}/>);
+
+        const message = await screen.findByText(/error/i);
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should render an error message when an unexpected error occurs', async () => {
+        server.use(http.get("/products/1", () => HttpResponse.error()));
+        render(<ProductDetail productId={1}/>);
+
+        const message = await screen.findByText(/error/i);
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should render a loading message when the products are being fetched', async () => {
+        server.use(http.get("/products/1", async () => {
+            await delay();
+            return HttpResponse.json([]);
+        }));
+
+        render(<ProductDetail productId={1}/>);
+
+        const message = await screen.findByText(/loading/i);
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should remove the loading indicator after data is fetched', async () => {
+        render(<ProductDetail productId={1}/>);
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    });
+
+    it('should remove the loading indicator if the data fetching failed', async () => {
+        server.use(http.get("/products/1", () => HttpResponse.error()));
+        render(<ProductDetail productId={1}/>);
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
     });
 });
--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -1,9 +1,10 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitForElementToBeRemoved} from "@testing-library/react";
 import ProductList from "../../src/components/ProductList.tsx";
 import {server} from "../mocks/server.ts";
-import {http, HttpResponse} from "msw";
+import {delay, http, HttpResponse} from "msw";
 import {afterAll, beforeAll} from "vitest";
 import {db} from "../mocks/db.ts";
+import {QueryClient, QueryClientProvider} from "react-query";
 
 describe('ProductList', () => {
     const productIds: number[] = [];
@@ -16,8 +17,24 @@ describe('ProductList', () => {
         db.product.deleteMany({where: {id: {in: productIds}}})
     });
 
+    const renderComponent = () => {
+        const client = new QueryClient({
+            defaultOptions: {
+                queries: {
+                    retry: false,
+                },
+            },
+        });
+
+        render(
+            <QueryClientProvider client={client}>
+                <ProductList/>
+            </QueryClientProvider>
+        );
+    }
+
     it('should render the product list', async () => {
-        render(<ProductList/>);
+        renderComponent();
 
         const items = await screen.findAllByRole("listitem");
         expect(items.length).toBeGreaterThan(0);
@@ -25,7 +42,7 @@ describe('ProductList', () => {
 
     it('should render no products available when there are no products', async () => {
         server.use(http.get("/products", () => HttpResponse.json([])));
-        render(<ProductList/>);
+        renderComponent();
 
         const message = await screen.findByText(/no products/i);
         expect(message).toBeInTheDocument();
@@ -33,10 +50,34 @@ describe('ProductList', () => {
 
     it('should render an error message when an unexpected error occurs', async () => {
         server.use(http.get("/products", () => HttpResponse.error()));
-        render(<ProductList/>);
+        renderComponent();
 
         const message = await screen.findByText(/error/i);
         expect(message).toBeInTheDocument();
     });
-})
-;
+
+    it('should render a loading message when the products are being fetched', async () => {
+        server.use(http.get("/products", async () => {
+            await delay();
+            return HttpResponse.json([]);
+        }));
+
+        renderComponent();
+
+        const message = await screen.findByText(/loading/i);
+        expect(message).toBeInTheDocument();
+    });
+
+    it('should remove the loading indicator after data is fetched', async () => {
+        renderComponent();
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    });
+
+    it('should remove the loading indicator if the data fetching failed', async () => {
+        server.use(http.get("/products", () => HttpResponse.error()));
+        renderComponent();
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    });
+});
